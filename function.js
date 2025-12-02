4a869d4488a51bd9fbea519a647e414766e8283e
Porchezhian <porchezhian2005@gmail.com>
2025-12-02 22:33:45 +0530

    Backend

---
+++ function.js
@@ -0,0 +1,35 @@
+// let n = 2;
+// function square(sum){
+//     let ans = sum * sum ;
+//     return ans
+// }
+// let square1 = square(n);
+// console.log(square1);
+// let square2 = square(3);
+// console.log(square2);
+
+/*...............................................................................*/ 
+let name ;
+
+function greetUser(name){
+    return `Hello ${name}`
+};
+
+let greetUSer1 = greetUser('Alax');
+let greetUser2 = greetUser('losheka');
+
+console.log(greetUser2);
+console.log(greetUSer1);
+
+let newUser = 'Lavanya';
+let message = `Hello ${newUser}, Welcome to our company Ms.${newUser}`;
+console.log(message);
+
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,15 @@
+const  a = 99;
+const  b = 88;
+try{
+    console.log(a);
+}
+catch(err){
+    console.log(`Error occurs:${err}`);
+}
+
+
+
+
+
+
+
